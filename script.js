76595c51dd2051966dfe026528039bdbd0b0c67d
Taushif Siddique <taushifsidd@gmail.com>
2023-06-17 16:31:39 +0530

    new commit

---
+++ script.js
@@ -0,0 +1,27 @@
+let a= document.querySelector(".box")
+let b= document.querySelector("#input1")
+let body = document.querySelector("body")
+
+function show(){
+    a.style.display="block"
+    body.style.blur="(10px)"
+
+}
+
+function hide(){
+    a.style.display="none"
+}
+
+// count=0;
+// function add(){
+//     b.style.display="block"
+//     c.style.display="block"
+//     count++;
+
+// }
+
+// function del(){
+//     b.style.display="none"
+//     c.style.display="none"
+// }
+
